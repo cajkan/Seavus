b64b2671f57b3e301d1b4d30738db74177b70292
arena-top100 <arena-top100@arenatop100-Aspire-E5-573>
2017-03-09 00:48:09 +0100

    Cas 2

---
+++ cas2.php
@@ -0,0 +1,17 @@
+<?php
+    $role = "admin";
+    
+    // Year, Month, Day - Hour, Minute, Seconds
+    $date = date("Y-m-d H:m:s");
+?>
+
+
+<?php if($role == "admin") { ?>
+HTML
+<?php } else { ?>
+HTML
+<?php } ?>
+
+
+
+    
---
+++ private/config.php
@@ -0,0 +1,7 @@
+<?php
+    // Server Configuration
+    define("host", "localhost");
+    define("db_user", "root");
+    define("db_password", "");
+    define("database", "users");
+?>
